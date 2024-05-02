88943b86e9ea9e897b3d23e492095c3c49c362c8
Thiago <thiagowendtpercyak@outlook.com>
2024-05-02 10:36:03 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
      return (
-          <h1>Oii mo, so queria falar que eu te amo mais que tudo, vc me faz querer ser melhor e me sentir muito melhor. Quero estar com voce sempre, te amo, vou te mimar muito ainda</h1>
+          <h1>Oii mo, so queria falar que eu te amo mais que tudo, vc me faz querer ser melhor e me sentir muito melhor. Quero estar com voce sempre, te amo, vou te mimar muito ainda! :D</h1>
      )
 }
 
