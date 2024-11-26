9bc41ffc032ca776ceeeb7ffa5746cfba51ece3e
Thiago Wendt Percyak <137214190+Thiag0WP@users.noreply.github.com>
2024-11-26 13:56:01 +0000

    Copia do primeiro index

---
+++ pages/indexx.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <h1>
+            Oii mo, so queria falar que eu te amo mais que tudo, vc me faz
+            querer ser melhor e me sentir muito melhor. Quero estar com voce
+            sempre, te amo, vou te mimar muito ainda! :D
+        </h1>
+    );
+}
+
+export default Home;
