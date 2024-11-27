5ab6caa174c4a41350944c80460b85e49ec2d5d7
Thiago Wendt Percyak <137214190+Thiag0WP@users.noreply.github.com>
2024-11-27 14:55:10 +0000

    Teste do `jest` e conclusão da issue `Testes Automatizados`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-    return (
-        <h1>
-            Oii mo, so queria falar que eu te amo mais que tudo, vc me faz
-            querer ser melhor e me sentir muito melhor. Quero estar com voce
-            sempre, te amo, vou te mimar muito ainda! :D
-        </h1>
-    );
+    return <h1>Em produção</h1>;
 }
 
 export default Home;
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,11 @@
-test("espero que 1 seja 1", () => {
-    expect(2).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+    const resultado = calculadora.somar(2, 2);
+    expect(resultado).toBe(4);
+});
+
+test("somar 2 + 2 deveria retornar 4", () => {
+    const resultado = calculadora.somar(68, 82);
+    expect(resultado).toBe(150);
 });
